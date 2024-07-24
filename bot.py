1729ba27d519cfe5d1b94704c309986ec5acd793
tykkmm <tykhanew45@gmail.com>
2024-07-24 17:50:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,62 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Suhaib-Rashid-Dar
+import asyncio
+import logging 
+import logging.config
+from database import db 
+from config import Config  
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer 
+from pyrogram.enums import ParseMode
+from pyrogram.errors import FloodWait 
 
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
-from config import Config
-import logging
-from pyrogram import Client, idle
-from pyromod import listen  # type: ignore
-from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+class Bot(Client): 
+    def __init__(self):
+        super().__init__(
+            Config.BOT_SESSION,
+            api_hash=Config.API_HASH,
+            api_id=Config.API_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=50,
+            bot_token=Config.BOT_TOKEN
+        )
+        self.log = logging
 
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        logging.info(f"{me.first_name} with for pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+        self.id = me.id
+        self.username = me.username
+        self.first_name = me.first_name
+        self.set_parse_mode(ParseMode.DEFAULT)
+        text = "**๏[-ิ_•ิ]๏ bot restarted !**"
+        logging.info(text)
+        success = failed = 0
+        users = await db.get_all_frwd()
+        async for user in users:
+           chat_id = user['user_id']
+           try:
+              await self.send_message(chat_id, text)
+              success += 1
+           except FloodWait as e:
+              await asyncio.sleep(e.value + 1)
+              await self.send_message(chat_id, text)
+              success += 1
+           except Exception:
+              failed += 1 
+    #    await self.send_message("venombotsupport", text)
+        if (success + failed) != 0:
+           await db.rmve_frwd(all=True)
+           logging.info(f"Restart message status"
+                 f"success: {success}"
+                 f"failed: {failed}")
 
-app = Client(
-    "bot",
-    class Config:
-    APP_ID = "class Config:
-    APP_ID = "29426486"
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    in_memory=True,
-    plugins=dict(root="Uploader"),
-)
-
-
-if __name__ == "__main__":
-    print("Starting the Bot...")
-    try:
-        app.start()
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
-    except AccessTokenInvalid:
-        raise Exception("Your BOT_TOKEN is not valid.")
-    uname = app.get_me().username
-    print(f"@{uname} started successfully !")
-    idle()
-    app.stop()
-    print("Bot stopped. Bye !")
+    async def stop(self, *args):
+        msg = f"@{self.username} stopped. Bye."
+        await super().stop()
+        logging.info(msg)

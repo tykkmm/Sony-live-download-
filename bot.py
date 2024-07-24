44d9af0234ed28bce990d93fb9c75e97fe6a3c93
tykkmm <tykhanew45@gmail.com>
2024-07-24 18:04:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging 
 import logging.config
-from database import db 
 from config import Config  
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer 

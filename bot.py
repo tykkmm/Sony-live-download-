4b50629361216ef8df417769a7371c4caf75acbf
tykkmm <tykhanew45@gmail.com>
2024-07-24 12:13:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
 
-
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 import os
-from Uploader.config import Config
-from pyrogram import Client as Tellyhub
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 if __name__ == "__main__" :
-    
+    # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(root="Uploader")
-    Warrior = Tellyhub("@UPLOADER_X_BOT",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    plugins=plugins)
-    Warrior.run()
+    plugins = dict(
+        root="plugins"
+    )
+    app = pyrogram.Client(
+        "X-URL-Uploader",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+    app.run()

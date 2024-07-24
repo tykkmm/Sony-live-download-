515c161f99164a497aa79a6d823ba9f9a3e8fd82
tykkmm <tykhanew45@gmail.com>
2024-07-24 15:41:56 +0500

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -3,6 +3,6 @@ import os
 class Config(object):
 
     #TELEGRAM STUFF
-    APP_ID = int(os.environ.get("APP_ID", "29426486"))
-    API_HASH = os.environ.get("API_HASH", "d71ad4ec048ab41677a1a439b21ff0c9")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To")
+    APP_ID = int(os.environ.get("29426486", ""))
+    API_HASH = os.environ.get("d71ad4ec048ab41677a1a439b21ff0c9", "")
+    BOT_TOKEN = os.environ.get("7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To", "")
